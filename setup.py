b372a776e17e0c03838cf81282991eb0d945eae0
Jakob Hoeper <jhoeper@bbaw.de>
2018-10-15 18:00:29 +0200

    rename package

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
+VERSION="0.1a1.dev1"
+
 setup(
-    name="aaew couchdb utils",
-    version="0.1",
+    name="aaew-couch-utils",
+    version=VERSION,
     packages=find_packages(),
 )
 
